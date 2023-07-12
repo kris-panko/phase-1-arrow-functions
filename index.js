d172e04d426a72b7ba86ac0a1a39f249f9f45e8d
kris-panko <krispanko@gmail.com>
2023-07-12 09:06:50 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function divide(a = 200, b = 100) {
+    return a / b;
+}
+
+const square = param1 => {
+    return param1 * param1;
+}
+
+const add = (param1, param2) =>{
+    return param1+ param2;
+}
